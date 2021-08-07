2fc14d107ae48e7650d3733fa1603753484c763d
Nicholas Hull <interfectum@gmail.com>
2021-08-06 17:33:54 -0700

    Changed to SDCC syntax, optimizations, separated PPI function and definitions

--- src/lib/zf_lcd.c
+++ src/lib/zf_lcd.c
@@ -1,39 +1,27 @@
 // ST7920 LCD controller functions for Z-Fighter
 // by Tangent 2021
 
-#define	PORTA	40h		
-#define	PORTB	41h
-#define	PORTC	42h
-#define CTRL	43h
-
-void ppi_init();
-void lcd_init();
 void lcd_update(unsigned char arr[]);
-void lcd_instruction(int i, int extended);
-void lcd_data(int d);
+void lcd_instruction(unsigned char i, unsigned char extended);
+void lcd_data(unsigned char d);
 void lcd_wait_busy();
 void lcd_switch_buffer();
 
-void lcd_init()
-	{
-		ppi_init();
-	}
-
-int buffer = 1;
+unsigned char buffer = 0;
 void lcd_update(unsigned char arr[])
 	{	
 		int upperScreenPointer = 0;
 		int lowerScreenPointer = 512;
 
-		int x = 0;
-		int y = 0;
+		unsigned char x = 0;
+		unsigned char y = 0;
 
 		if (buffer == 1)
 			{
 				y = 32;
 			}
 
-		for (int a=0; a<=31; a++)
+		for (unsigned char a=0; a<=31; a++)
 			{
 				lcd_instruction(y+0x80, 1);
 				lcd_instruction(x+0x80, 1);
@@ -42,7 +30,7 @@ void lcd_update(unsigned char arr[])
 						lcd_data(arr[upperScreenPointer]);
 						upperScreenPointer++;
 					}
-				for (int i=0; i<=15; i++)
+				for (unsigned char i=0; i<=15; i++)
 					{
 						lcd_data(arr[lowerScreenPointer]);
 						lowerScreenPointer++;
@@ -54,52 +42,52 @@ void lcd_update(unsigned char arr[])
 		lcd_switch_buffer();
 	}
 
-int instruction;
-void lcd_instruction(int i, int extended)
+unsigned char instruction;
+void lcd_instruction(unsigned char i, unsigned char extended)
 	{
 		instruction = i;
 
-		if (extended == 1)				// Check basic or extended instruction
+		if (extended == 1)					// Check basic or extended instruction
 			{	
 				lcd_wait_busy();
-				#asm
+				__asm
 				LD 		A,36h				// Set extended instruction set
 				OUT 	(PORTB),A
 				CALL 	_lcd_enable
-				#endasm
+				__endasm;
 
 				lcd_wait_busy();
-				#asm
+				__asm
 				LD  	A,(_instruction)	// Send extended instruction
 				OUT 	(PORTB),A 
 				CALL 	_lcd_enable
-				#endasm
+				__endasm;
 
 				lcd_wait_busy();
-				#asm
+				__asm
 				LD  	A,30h				// Reset basic instruction set
 				OUT 	(PORTB),A
-				call 	_lcd_enable
-				#endasm
+				CALL	_lcd_enable
+				__endasm;
 			}
 		else								
 			{
 				lcd_wait_busy();
-				#asm
+				__asm
 				LD		A,(_instruction)	// Send basic instruction
 				OUT 	(PORTB),A
 				CALL	_lcd_enable
-				#endasm
+				__endasm;
 			}
 	}
 
-int data;
-void lcd_data(int d)
+unsigned char data;
+void lcd_data(unsigned char d)
 	{
 		data = d;
 		// lcd_wait_busy();			// Disable for speed boost (possibly unstable)
 
-		#asm 
+		__asm 
 		LD  	A,5 				// Set LCD D
 		OUT 	(CTRL),A
 
@@ -109,32 +97,22 @@ void lcd_data(int d)
 
 		LD  	A,4 				// Reset LCD #I
 		OUT 	(CTRL),A
-		#endasm
-	}
-
-void ppi_init()
-	{
-		#asm
-		LD		A,90h				// 8255 Simple I/O, PA in, PB,C out
-		OUT		(CTRL),A
-		LD		A,7					// Backlight ON
-		OUT		(CTRL),A
-		#endasm
+		__endasm;
 	}
 
 void lcd_enable()
 	{
-		#asm
+		__asm
 		LD		A,1		
 		OUT		(CTRL),A
 		LD	 	A,0
 		OUT 	(CTRL),A
-		#endasm
+		__endasm;
 	}
 
 void lcd_wait_busy()
 	{
-		#asm 
+		__asm 
 		LD  	A,92h 				// 8255 Simple I/O, PA,B in, PC out
 		OUT 	(CTRL),A
 		LD 		A,7					// Backlight ON
@@ -145,18 +123,18 @@ void lcd_wait_busy()
 		LD		A,1		 			// Set LCD Enable
 		OUT		(CTRL),A	
 
-		BUSYLOOP:
+		0001$:
 			IN 		A,(PORTB)
 			BIT 	7,A 			// Check busy flag
-			JR 		NZ,BUSYLOOP
+			JR 		NZ,0001$
 
 		LD	 	A,0					// Reset LCD Enable
 		OUT 	(CTRL),A
 		LD		A,2 				// Reset LCD #WR
 		OUT		(CTRL),A
-		#endasm
 
-		ppi_init();					// Reset PPI
+		CALL	_ppi_init
+		__endasm;
 	}
 
 void lcd_switch_buffer()
@@ -168,9 +146,9 @@ void lcd_switch_buffer()
 			}
 		else
 			{
-				lcd_instruction(0+0x40, 1);	// Vertical scroll address = GDRAM bottom half
+				lcd_instruction(0+0x40, 1);		// Vertical scroll address = GDRAM bottom half
 				buffer = 1;
 			}
 
-		lcd_instruction(0x03, 1); // Enable vertical scroll position
+		lcd_instruction(0x03, 1);				// Enable vertical scroll position
 	}
