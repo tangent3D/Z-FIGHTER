98c3acb2eb32b94c08b2fa067cfdb68df915d42f
Nicholas Hull <interfectum@gmail.com>
2021-08-12 02:09:55 -0700

    Updated keypad functions

---
+++ src/lib/zf_keys.c
@@ -0,0 +1,28 @@
+// Keypad input functions for Z-Fighter
+// by Tangent 2021
+
+#include "zf_keys.h"
+
+unsigned char keyGet() __naked
+	{
+		__asm
+		IN 		A,(PORTA)		// Read the current state of the keypad
+		LD		L,A 			// Return value
+		RET
+		__endasm;
+	}
+
+void keyWait(unsigned char key) __naked
+	{
+		(void)key;
+		__asm 
+		LD      IY,2            // Bypass return address of function
+        ADD     IY,SP
+        LD      D,(IY)          // Load parameter (key) into D			
+        KWLOOP:
+    	IN 		A,(PORTA) 		// Read the current state of the keypad
+    	AND  	D 				// Check if key is pressed
+    	JR		NZ,KWLOOP		// Repeat loop until key is pressed
+		RET
+		__endasm;
+	}
--- src/lib/zf_keys.h
+++ src/lib/zf_keys.h
@@ -1,4 +1,4 @@
-// Keypad input functions for Z-Fighter
+// Keypad definitions for Z-Fighter
 // by Tangent 2021
 
 #ifndef ZF_KEYS_H
@@ -7,41 +7,31 @@
 #include "zf_ppi.h"
 
 // Key definitions
-#define KEY_UP		1
-#define KEY_DOWN	2
-#define KEY_LEFT	4
-#define KEY_RIGHT	8
-#define KEY_A		16
-#define KEY_B		32
-#define KEY_C		64
-#define KEY_D		128
+#define KEY_UP		1 	// Port A bit 0
+#define KEY_DOWN	2 	//   ''   bit 1
+#define KEY_LEFT	4 	// 	 ''   bit 2
+#define KEY_RIGHT	8 	// 	 ''   bit 3
+#define KEY_A		16  // 	 ''   bit 4
+#define KEY_B		32 	// 	 ''   bit 5
+#define KEY_C		64  //	 ''   bit 6
+#define KEY_D		128 // 	 ''   bit 7
 
-/* 
-Keypad layout:
-+-----+-----+-----+-----+-----+
-|     |  U  |     |  A  |  B  |
-|     |     |     |     |     |
-|  L  +-----+  R  +-----+-----+
-|     |  D  |     |  C  |  D  |
-|     |     |     |     |     |
-+-----+-----+-----+-----+-----+
-*/
+// Keys are pulled high with pull-up resistors and
+// are in a logic '1' state when not depressed.
 
-unsigned char keyGet() __naked
-	{
-		__asm
-		IN 		A,(PORTA)	// Read the current state of the keypad
-		LD		L,A 		// Return value
-		RET
-		__endasm;
-	}
+// Keypad layout:
+// +-----+-----+-----+-----+-----+
+// |     |  U  |     |  A  |  B  |
+// |     |     |     |     |     |
+// |  L  +-----+  R  +-----+-----+
+// |     |  D  |     |  C  |  D  |
+// |     |     |     |     |     |
+// +-----+-----+-----+-----+-----+
 
-void keyWait() __naked
-	{
-		__asm 
+// Return the current state of the keypad keys
+unsigned char keyGet();
 
-		RET
-		__endasm;
-	}
+// Wait until a specific key is pressed (ex. 'KEY_UP')
+void keyWait(unsigned char key);
 
 #endif
