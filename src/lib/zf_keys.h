2328008f768c9c833cf3c7ffa50c1292a762b6cc
Nicholas Hull <40728628+tangent3D@users.noreply.github.com>
2021-08-12 02:27:20 -0700

    Update zf_keys.h

--- src/lib/zf_keys.h
+++ src/lib/zf_keys.h
@@ -7,14 +7,14 @@
 #include "zf_ppi.h"
 
 // Key definitions
-#define KEY_UP		1 	// Port A bit 0
-#define KEY_DOWN	2 	//   ''   bit 1
-#define KEY_LEFT	4 	// 	 ''   bit 2
-#define KEY_RIGHT	8 	// 	 ''   bit 3
-#define KEY_A		16  // 	 ''   bit 4
-#define KEY_B		32 	// 	 ''   bit 5
-#define KEY_C		64  //	 ''   bit 6
-#define KEY_D		128 // 	 ''   bit 7
+#define KEY_UP      1   // Port A bit 0
+#define KEY_DOWN    2   //   ''   bit 1
+#define KEY_LEFT    4   //   ''   bit 2
+#define KEY_RIGHT   8   //   ''   bit 3
+#define KEY_A       16  //   ''   bit 4
+#define KEY_B       32  //   ''   bit 5
+#define KEY_C       64  //   ''   bit 6
+#define KEY_D       128 //   ''   bit 7
 
 // Keys are pulled high with pull-up resistors and
 // are in a logic '1' state when not depressed.
