66f8661a7c0853caae3e6481e0ca6dad889cd389
Nicholas Hull <40728628+tangent3D@users.noreply.github.com>
2021-08-12 02:27:49 -0700

    Update zf_keys.c

--- src/lib/zf_keys.c
+++ src/lib/zf_keys.c
@@ -1,28 +1,28 @@
-// Keypad input functions for Z-Fighter
+A// Keypad input functions for Z-Fighter
 // by Tangent 2021
 
 #include "zf_keys.h"
 
 unsigned char keyGet() __naked
-	{
-		__asm
-		IN 		A,(PORTA)		// Read the current state of the keypad
-		LD		L,A 			// Return value
-		RET
-		__endasm;
-	}
+    {
+        __asm
+        IN      A,(PORTA)       // Read the current state of the keypad
+        LD      L,A             // Return value
+        RET
+        __endasm;
+    }
 
 void keyWait(unsigned char key) __naked
-	{
-		(void)key;
-		__asm 
-		LD      IY,2            // Bypass return address of function
+    {
+        (void)key;
+        __asm 
+        LD      IY,2            // Bypass return address of function
         ADD     IY,SP
-        LD      D,(IY)          // Load parameter (key) into D			
+        LD      D,(IY)          // Load parameter (key) into D          
         KWLOOP:
-    	IN 		A,(PORTA) 		// Read the current state of the keypad
-    	AND  	D 				// Check if key is pressed
-    	JR		NZ,KWLOOP		// Repeat loop until key is pressed
-		RET
-		__endasm;
-	}
+        IN      A,(PORTA)       // Read the current state of the keypad
+        AND     D               // Check if key is pressed
+        JR      NZ,KWLOOP       // Repeat loop until key is pressed
+        RET
+        __endasm;
+    }
