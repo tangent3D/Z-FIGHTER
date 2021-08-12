a21fdf75d41e2f2d2091b17d8072af4b3ace6340
Nicholas Hull <interfectum@gmail.com>
2021-08-12 02:30:20 -0700

    Update zf_ppi.h

--- src/lib/zf_ppi.h
+++ src/lib/zf_ppi.h
@@ -5,17 +5,17 @@
 #ifndef ZF_PPI_H
 #define ZF_PPI_H
 
-#define PORTA   40h     	// Connected to key switches
-#define PORTB   41h     	// Connected to ST7920 LCD data bus
-#define PORTC   42h     	// Connected to LCD control signals, backlight and buzzer
-#define CTRL    43h     	// 8255 control port
+#define PORTA   40h         // Connected to key switches
+#define PORTB   41h         // Connected to ST7920 LCD data bus
+#define PORTC   42h         // Connected to LCD control signals, backlight and buzzer
+#define CTRL    43h         // 8255 control port
 
 // BSR (Bit Set/Reset) control words:
-#define LCD_EN_LO	0x0     // LCD 'E' (Enable) high       
-#define LCD_EN_HI	0x1     // LCD 'E' low
-#define LCD_WR		0x2     // LCD R/#W low (Write)
-#define LCD_RD		0x3     // LCD R/#W high (Read)
-#define LCD_INST	0x4     // LCD RS low (Instruction)
+#define LCD_EN_LO   0x0     // LCD 'E' (Enable) high       
+#define LCD_EN_HI   0x1     // LCD 'E' low
+#define LCD_WR      0x2     // LCD R/#W low (Write)
+#define LCD_RD      0x3     // LCD R/#W high (Read)
+#define LCD_INST    0x4     // LCD RS low (Instruction)
 #define LCD_DATA    0x5     // LCD RS high (Data)
 #define LCD_BL_OFF  0x6     // Backlight off
 #define LCD_BL_ON   0x7     // Backlight on
