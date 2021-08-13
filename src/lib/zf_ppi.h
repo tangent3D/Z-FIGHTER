459e7f4d819e021cf1dc0561934dcbfe3ce36ca2
Nicholas Hull <40728628+tangent3D@users.noreply.github.com>
2021-08-13 11:45:45 -0700

    Update zf_ppi.h

--- src/lib/zf_ppi.h
+++ src/lib/zf_ppi.h
@@ -5,10 +5,10 @@
 #ifndef ZF_PPI_H
 #define ZF_PPI_H
 
-#define PORTA   40h         // Connected to key switches
-#define PORTB   41h         // Connected to ST7920 LCD data bus
-#define PORTC   42h         // Connected to LCD control signals, backlight and buzzer
-#define CTRL    43h         // 8255 control port
+#define PORTA       40h     // Connected to key switches
+#define PORTB       41h     // Connected to ST7920 LCD data bus
+#define PORTC       42h     // Connected to LCD control signals, backlight and buzzer
+#define CTRL        43h     // 8255 control port
 
 // BSR (Bit Set/Reset) control words:
 #define LCD_EN_LO   0x0     // LCD 'E' (Enable) high       
