7cf9c7fa6916882de3565ccca4aa235c05010c2f
Nicholas Hull <interfectum@gmail.com>
2021-08-12 20:53:54 -0700

    Initial submit of util functions

---
+++ src/lib/zf_util.h
@@ -0,0 +1,19 @@
+// Utility functions for Z-Fighter
+// by Tangent 2021
+
+#ifndef ZF_UTIL_H
+#define ZF_UTIL_H
+
+// Sleep for approximately 1s
+void sleepSec();
+
+// Sleep for approximately 500ms
+void sleepHlfSec();
+
+// Sleep for approximately 250ms
+void sleepQtrSec();
+
+// Sleep for approximately 125ms
+void sleepEgtSec();
+
+#endif
