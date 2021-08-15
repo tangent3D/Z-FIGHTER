58a215cc6efe76997f047f1b822a3dfa421b69b4
Nicholas Hull <interfectum@gmail.com>
2021-08-15 15:48:36 -0700

    PPI lib

---
+++ src/lib/zf_ppi_user.h
@@ -0,0 +1,13 @@
+// 8255A Programmable Peripheral Interface definitions for Z-Fighter
+// (User parallel port)
+// by Tangent 2021
+
+#ifndef ZF_PPI_H
+#define ZF_PPI_H
+
+#define USERPORTA       80h     // PA7-PA0
+#define USERPORTB       81h     // PB7-PB0
+#define USERPORTC       82h     // Connected to LCD control signals, backlight and buzzer
+#define USERCTRL        83h     // User PPI control port
+
+#endif
