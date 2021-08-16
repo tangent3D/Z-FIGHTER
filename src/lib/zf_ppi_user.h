b1a789b81b753fa3d17ea1cd26abafa7aa32f791
Nicholas Hull <interfectum@gmail.com>
2021-08-16 02:34:45 -0700

    Update zf_ppi_user.h

--- src/lib/zf_ppi_user.h
+++ src/lib/zf_ppi_user.h
@@ -5,9 +5,9 @@
 #ifndef ZF_PPI_H
 #define ZF_PPI_H
 
-#define USERPORTA       80h     // PA7-PA0
-#define USERPORTB       81h     // PB7-PB0
-#define USERPORTC       82h     // Connected to LCD control signals, backlight and buzzer
-#define USERCTRL        83h     // User PPI control port
+#define USERPORTA	80h     // PA7-PA0
+#define USERPORTB   81h     // PB7-PB0
+#define USERPORTC   82h     // Connected to LCD control signals, backlight and buzzer
+#define USERCTRL    83h     // User PPI control port
 
 #endif
