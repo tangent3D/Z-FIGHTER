452fd7e64c89ca56c1f3c44c94c9d55fe99add2e
Nicholas Hull <interfectum@gmail.com>
2021-08-21 03:55:27 -0700

    Update z88dk_embedded_z80.bat

--- src/toolchain/z88dk_embedded_z80.bat
+++ src/toolchain/z88dk_embedded_z80.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-REM Compile a .c source file to an Embedded_Z80 binary with z88dk/zsdcc
+REM Compile a .c source file to an Embedded_Z80 ROM model binary with z88dk/zsdcc
 
 CALL %~dp0\config.bat
 
@@ -9,4 +9,19 @@ SET mysource=%~n1
 
 ECHO [Compiling %mysource%.c with z88dk for Embedded_Z80]
 
-zcc +z80 -I%INC% -L%LIBPATH% -lzf_lib_embedded_z80 -o "%mysource%.com" %mysource%.c -compiler=sdcc
+zcc +z80 -vn -SO3 -startup=1 -clib=sdcc_iy --max-allocs-per-node200000 -I%INC% -L%LIBPATH% -lzf_lib_embedded_z80 %mysource%.c -o "%mysource%" -lm -create-app
+
+REM Concatenate output binaries
+COPY /B %mysource%_CODE.bin + %mysource%_DATA.bin %mysource%.bin
+
+REM Clean up outputs, REM if debugging
+del %mysource%_CODE.bin >nul 2>&1
+del %mysource%_DATA.bin >nul 2>&1
+del %mysource%_BSS.bin >nul 2>&1
+del %mysource%_UNASSIGNED.bin >nul 2>&1
+
+if %ERRORLEVEL% == 0 (
+    echo [Compilation succeeded!]
+) else if %ERRORLEVEL% == 1 (
+    echo [Compilation failed!]
+)
