1d38e0913b6ca9fcb198b3b3f54d670c3f55d87c
Nicholas Hull <interfectum@gmail.com>
2021-08-23 18:19:58 -0700

    Changed to newlib

--- src/lib/zf_lib.bat
+++ src/lib/zf_lib.bat
@@ -5,7 +5,7 @@ REM Compile Z-Fighter library source files to .lib
 CALL %~dp0\..\toolchain\config.bat
 
 REM Compile library for Embedded_Z80 target
-zcc +z80 -I%INC% -x -o zf_lib_embedded_z80 -compiler=sdcc @zf_lib.lst
+zcc +z80 -SO3 -clib=sdcc_iy --max-allocs-per-node200000 -I%INC% -x -o zf_lib_embedded_z80 @zf_lib.lst
 
 REM Compile library for CP/M target
 zcc +cpm -SO3 -clib=sdcc_iy --max-allocs-per-node200000 -I%INC% -x -o zf_lib_cpm @zf_lib.lst
