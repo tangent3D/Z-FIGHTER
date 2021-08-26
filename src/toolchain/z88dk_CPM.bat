ed04be49b0f123f08883f6c72847733a3353b8d9
Nicholas Hull <interfectum@gmail.com>
2021-08-25 21:44:37 -0700

    Added ASM output .bat file for debugging

--- src/toolchain/z88dk_CPM.bat
+++ src/toolchain/z88dk_CPM.bat
@@ -9,13 +9,14 @@ SET mysource=%~n1
 
 ECHO [Compiling %mysource%.c with z88dk for CP/M]
 
+REM Add "-lm" to link in generic Z80 maths library if required
 zcc +cpm -SO3 -clib=sdcc_iy --max-allocs-per-node200000 -I%INC% -L%LIBPATH% -lzf_lib_cpm -o "%mysource%.com" %mysource%.c -create-app
 
 REM Send executable to Z-Fighter only if compilation succeeds
 if %ERRORLEVEL% == 0 (
     echo [Compilation succeeded!]
     ZF_XMODEM %cd%\%mysource%.com %mysource%.com
-) else if %ERRORLEVEL% == 1 (
+) else if %ERRORLEVEL% == 1 (b
     echo [Compilation failed!]
 )
 
