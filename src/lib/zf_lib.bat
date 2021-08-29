026b34a516d88f5247fef4dd1ebc811ed102e5cb
Nicholas Hull <interfectum@gmail.com>
2021-08-29 16:05:18 -0700

    Update zf_lib.bat

--- src/lib/zf_lib.bat
+++ src/lib/zf_lib.bat
@@ -6,7 +6,7 @@ CALL %~dp0\..\toolchain\config.bat
 
 REM Compile library for Embedded_Z80 target
 ECHO Compiling Z-Fighter libs for Embedded_Z80 target...
-zcc +z80 -SO3 -clib=sdcc_iy --max-allocs-per-node200000 -I%INC% -x -o zf_lib_embedded_z80 @zf_lib.lst
+zcc +z80 -SO3 -clib=sdcc_iy --max-allocs-per-node200000 -I%INC% -x -o zf_lib_z80 @zf_lib.lst
 
 REM Compile library for CP/M target
 ECHO Compiling Z-Fighter libs for CP/M target...
