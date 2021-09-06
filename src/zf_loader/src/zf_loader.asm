aa187af474d73a5d3afda8679181ef16702b66ac
Nicholas Hull <40728628+tangent3D@users.noreply.github.com>
2021-09-05 19:54:30 -0700

    Minor optimizations

--- src/zf_loader/src/zf_loader.asm
+++ src/zf_loader/src/zf_loader.asm
@@ -27,10 +27,7 @@ LOADER:
     LD      A,0
     OUT     (38h),A                ; Disable ROM
 
-    LD      C,SIO_AC               ; Load C with SIO Ch.A control port
     CALL    INITSIO                ; Initialize SIO Ch.A
-    LD      C,SIO_BC               ; Load C with SIO Ch.B control port
-    CALL    INITSIO                ; Initialize SIO Ch.B
 
     LD      HL,GREETING            ; Write greeting messages
     CALL    STRING
@@ -43,10 +40,7 @@ LOADER:
     CALL    STRING
 
 EXECUTE:
-    LD      C,SIO_AC               ; Load C with SIO Ch.A control port
-    CALL    STOPSIO                ; Reset SIO Ch.A
-    LD      C,SIO_BC               ; Load C with SIO Ch.B control port
-    CALL    STOPSIO                ; Reset SIO Ch.B
+    CALL    RESTSIO                ; Reset SIO Ch.A and allow program to configure SIO
 
     CALL    DELAY
     CALL    0000h                  ; CALL loaded data
@@ -55,13 +49,15 @@ EXECUTE:
     JP      LOADER                 ; Allow program to RET to boot loader
 
 INITSIO:
-    LD      HL,SIOCFG              ; Load HL with base address of config table
+    LD      C,SIO_AC               ; Load C with SIO Ch.A control port
+    LD      HL,INITCFG             ; Load HL with base address of config table
     LD      B,11                   ; Load B with length of config table
     OTIR                           ; Output configuration table to (C)
     RET
 
-STOPSIO:
-    LD      HL,SIOCFG2             ; Load HL with base address of config table
+RESTSIO:
+    LD      C,SIO_AC               ; Load C with SIO Ch.A control port
+    LD      HL,RESTCFG             ; Load HL with base address of config table
     LD      B,4                    ; Load B with length of config table
     OTIR                           ; Output configuration table to (C)
     RET
@@ -70,7 +66,7 @@ STRING:
     LD      C,SIO_AC
 ST1:
     LD      A,(HL)                 ; Load character to be sent into A
-    CP      255                    ; Check if character = 255
+    OR      A                      ; Check if character = 0
     JP      Z,CRLF                 ; If so, insert CR,LF and exit routine
     CALL    TXCHAR                 ; Otherwise, transmit character
     INC     HL                     ; Increment HL to next character in string
@@ -130,36 +126,36 @@ RD3:
     INI                            ; Load (HL) with (C), INC HL
     JP      RD2                    ; Repeat until timeout occurs
 
-DELAY:                             ; Short delay
+DELAY:                             
     LD      B,255
-DL:                  
-    DJNZ    DL                     ; Repeat delay loop until B = 0
+DLOOP:                  
+    DJNZ    DLOOP                  ; Repeat delay loop until B = 0
     RET
 
-SIOCFG:
+INITCFG:
     DB      00011000b              ; WR0, Channel Reset
     DB      00010000b              ; WR0, Reset Ext/Status Interrupts
     DB      00101000b              ; WR0, Reset TxINT Pending   
-    DB      00110100b              ; WR0, Error Reset, select R4
-    DB      11000100b              ; WR4, x64 Clock Mode, 1 Stop Bit/Character, Parity Disable
-    DB      00000011b              ; WR0, select R3
-    DB      11000001b              ; WR3, Rx 8 Bits/Character, Auto Enables off, Rx Enable
-    DB      00000101b              ; WR0, select R5
-    DB      01101000b              ; WR5, No DTR, TX 8 Bits/Character, No Send Break, Tx Enable, RTS off (D1)
     DB      00000001b              ; WR0, select WR1
     DB      00000000b              ; WR1, Disable WAIT/READY, Disable Rx INT, Disable Tx INT
+    DB      00000011b              ; WR0, select R3
+    DB      11000001b              ; WR3, Rx 8 Bits/Character, Auto Enables off, Rx Enable
+    DB      00110100b              ; WR0, Error Reset, select R4
+    DB      11000100b              ; WR4, x64 Clock Mode, 1 Stop Bit/Character, Parity Disable
+    DB      00000101b              ; WR0, select R5
+    DB      01101000b              ; WR5, No DTR, TX 8 Bits/Character, No Send Break, Tx Enable, RTS off
 
-SIOCFG2:
+RESTCFG:
     DB      1                      ; WR0, select R3
     DB      0                      ; WR3, Rx Disable
     DB      5                      ; WR0, select R5
     DB      0                      ; WR5, Tx Disable
 
 GREETING:
-    DB      "[Z-Fighter Serial Boot Loader v0.1]",255
+    DB      "[Z-Fighter Serial Boot Loader v0.1]",0
 
 REQUEST:
-    DB      "Ready to load .BIN at 0000h.",255
+    DB      "Ready to load .BIN at 0000h.",0
 
 CONFIRM:
-    DB      "Transfer complete.",255
+    DB      "Transfer complete.",0
