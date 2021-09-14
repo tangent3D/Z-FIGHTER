d9065ce2966a232249d05796a4007229810d7b68
Nicholas Hull <interfectum@gmail.com>
2021-09-13 21:34:10 -0700

    Bitmap text lib and character set

---
+++ src/lib/src/zf_text.h
@@ -0,0 +1,11 @@
+// Bitmap text definitions for Z-Fighter
+// by Tangent 2021
+
+#ifndef ZF_TEXT_H
+#define ZF_TEXT_H
+
+void print(unsigned char string[], unsigned char x, unsigned char y);
+
+extern const unsigned char charSet[];
+
+#endif
