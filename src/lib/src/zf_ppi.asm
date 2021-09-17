84900f314a7c44f7bf9bfa956cb1a2727a796bca
Nicholas Hull <interfectum@gmail.com>
2021-09-17 01:54:11 -0700

    Deprecated zf_ppi, renamed zf_ppi_user to zf_ppi

--- src/lib/src/zf_ppi.asm
+++ src/lib/src/zf_ppi.asm
@@ -1,13 +1,16 @@
-; 8255A Programmable Peripheral Interface implementation for Z-Fighter peripherals
-; (Keys, LCD, buzzer)
+; 8255A Programmable Peripheral Interface implementation for Z-Fighter
+; (User parallel port)
 ; by Tangent 2021
 
 SECTION code_user
 
-EXTERN CTRL
-
-PUBLIC _ppiInit
-_ppiInit:
-    LD      A,90h           ; 8255 Simple I/O, PA in, PB,C out
-    OUT     (CTRL),A
+PUBLIC _byteReverse                 
+_byteReverse:                       ; Uses FASTCALL. L = byte
+    LD      A,L                     ; Load A with parameter (byte)
+    LD      B,8
+BRLOOP:
+    RL      L                       ; Reverse all bits in A
+    RRA
+    DJNZ    BRLOOP
+    LD      L,A                     ; Return reversed byte
     RET
