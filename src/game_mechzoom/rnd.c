9df80e92d0d0df7a8ba1c9dd21e6867d721a9f34
xrrawva <67735592+xrrawva@users.noreply.github.com>
2021-12-01 04:01:41 +0100

    wip

--- src/game_mechzoom/rnd.c
+++ src/game_mechzoom/rnd.c
@@ -7,14 +7,14 @@
 unsigned char rnd(unsigned char maxValue)
 {
     unsigned char mask;
-    if     (maxValue<1+ 1               )mask=1;
-    else if(maxValue<1+ 1+2             )mask=1+2;
-    else if(maxValue<1+ 1+2+4           )mask=1+2+4;
-    else if(maxValue<1+ 1+2+4+8         )mask=1+2+4+8;
-    else if(maxValue<1+ 1+2+4+8+16      )mask=1+2+4+8+16;
-    else if(maxValue<1+ 1+2+4+8+16+32   )mask=1+2+4+8+16+32;
-    else if(maxValue<1+ 1+2+4+8+16+32+64)mask=1+2+4+8+16+32+64;
-    else                                 mask=1+2+4+8+16+32+64+128; // 255
+    if     (maxValue<(unsigned char)(1+ 1               ))mask=1;                    // 00000001
+    else if(maxValue<(unsigned char)(1+ 1+2             ))mask=1+2;                  // 00000011
+    else if(maxValue<(unsigned char)(1+ 1+2+4           ))mask=1+2+4;                // 00000111
+    else if(maxValue<(unsigned char)(1+ 1+2+4+8         ))mask=1+2+4+8;              // 00001111
+    else if(maxValue<(unsigned char)(1+ 1+2+4+8+16      ))mask=1+2+4+8+16;           // 00011111
+    else if(maxValue<(unsigned char)(1+ 1+2+4+8+16+32   ))mask=1+2+4+8+16+32;        // 00111111
+    else if(maxValue<(unsigned char)(1+ 1+2+4+8+16+32+64))mask=1+2+4+8+16+32+64;     // 01111111
+    else                                                  mask=1+2+4+8+16+32+64+128; // 11111111
 
     unsigned char result;
     do
@@ -26,7 +26,7 @@ unsigned char rnd(unsigned char maxValue)
     return result;
 }
 
-//void rndImprove(unsigned char unpredictableByte)
-//{
-//    srand(rand()+unpredictableByte);
-//}
+void rndImprove(unsigned char unpredictableByte)
+{
+    srand(rand()+unpredictableByte);
+}
