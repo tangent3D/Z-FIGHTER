16d48cfd32bb682cb3e2b24c6a05c4b780c0366e
xrrawva <67735592+xrrawva@users.noreply.github.com>
2021-12-01 03:09:45 +0100

    wip

--- src/game_mechzoom/mechzoom.c
+++ src/game_mechzoom/mechzoom.c
@@ -24,17 +24,22 @@
 #define DEMON        8
 
 // tilesets
-const unsigned char tilesetFrame0[]={0,0,0,0,0,0,0,0,                 // EMPTY
-                                     243,64,4,48,3,64,12,126,         // WALL_PIECES
-                                     255,227,15,127,7,193,31,255,     // WALL_BROKEN
-                                     255,255,255,255,255,255,255,255, // WALL
-                                     0,8,28,8,20,0,0,0,               // MECH_NONE
-                                     223,77,231,30,15,37,216,144,     // MECH
-                                     223,109,247,78,223,27,48,60,     // MECH_ARMORED
-                                     0,0,0,255,255,0,0,0,             // BULLET
-                                     14,108,200,48,56,40,46,2};       // DEMON
+const unsigned char tilesetFrame0[]={0,0,0,0,0,0,0,0,                  // EMPTY
+                                     0,32,48,0,0,6,14,0,               // WALL_PIECES rock
+                                     0,57,115,7,175,63,255,126,        // WALL_BROKEN rock
+                                     126,255,255,255,255,255,255,126,  // WALL        rock
+                                     0,0,8,28,8,20,0,0,                // MECH_NONE
+                                     223,77,231,30,15,37,216,144,      // MECH
+                                     223,109,247,78,223,27,48,60,      // MECH_ARMORED
+                                     0,0,0,255,255,0,0,0,              // BULLET
+                                     14,108,200,48,56,40,46,2};        // DEMON
+// TODO: animation vs level skins
+const unsigned char tilesetFrame1[]={0,0,0,0,0,0,0,0,                  // EMPTY
+                                     0,48,192,4,4,4,8,0,               // WALL_PIECES metal
+                                     36,36,255,36,36,255,36,36,        // WALL_BROKEN metal
+                                     255,189,255,255,255,255,189,255}; // WALL        metal
 
-void scroll()
+void scrollAll()
 {
     tilemap[16*1]=0;
     tilemap[16*2]=0;
@@ -51,37 +56,77 @@ void scroll()
 }
 
 unsigned char simY=255;
+
+
+unsigned char scrollDelay=4;
+unsigned char scrollDelayCounter=scrollDelay;
+
+//unsigned char scrollY=255;
+void scroll()
+{
+    if(scrollDelayCounter)return;
+//    simY++;
+  //  if(simY==8)simY=0;
+
+    unsigned char* tilemapPointerTo=(simY<<4)+tilemap;
+    unsigned char* tilemapPointerFrom=tilemapPointerTo+1;
+    for(unsigned char x=0;x<15;x++)
+    {
+        (*tilemapPointerTo)=(*tilemapPointerFrom);
+        tilemapPointerTo++;
+        tilemapPointerFrom++;
+    }
+
+    unsigned char newTile=EMPTY;
+    if(rnd(16)==0)newTile=DEMON;
+    if(rnd(8)==0)newTile=WALL;
+    (*tilemapPointerTo)=newTile;
+
+
+
+
+
+}
+
 void sim()
 {
     simY++;
-    if(simY==8)simY=0;
+    if(simY==8)
+        {
+        simY=0;
 
-    unsigned char* tilemapPointer=(simY<<4)+tilemap;
-    for(unsigned char x=1;x<15;x++)
+        if(scrollDelayCounter==0)scrollDelayCounter=scrollDelay;
+        scrollDelayCounter--;
+        }
+
+    scroll();
+
+    unsigned char* tilemapPointer=(simY<<4)+tilemap+15;
+    for(unsigned char x=15;x<16;x--)
     {
         unsigned char tile=(*tilemapPointer);
         if(tile==BULLET)
         {
             (*tilemapPointer)=EMPTY;
-            unsigned char* tilemapPointer2=tilemapPointer;
-            tilemapPointer2++;
-            unsigned char tile=(*tilemapPointer2);
-            if(tile==EMPTY)
+            if(x<15)
             {
-                (*tilemapPointer2)=BULLET;
+                unsigned char* tilemapPointer2=tilemapPointer+1;
+                unsigned char tile=(*tilemapPointer2);
+                if(tile==EMPTY)
+                {
+                    (*tilemapPointer2)=BULLET;
+                }
+                else if(tile<WALL+1)
+                {
+                    (*tilemapPointer2)=tile-1;
+                }
+                else
+                {
+                    (*tilemapPointer2)=WALL_PIECES; // TODO: EXPLOSION?
+                }
+                //x++;
+                //tilemapPointer++;
             }
-            else if(tile<WALL+1)
-            {
-                (*tilemapPointer2)=tile-1;
-            }
-            else if(tile<WALL+1)
-            {
-                (*tilemapPointer2)=WALL_PIECES; // TODO: EXPLOSION
-            }
-
-
-            x++;
-            tilemapPointer++;
         }
         else if(tile==DEMON)
         {
@@ -92,29 +137,26 @@ void sim()
             if(tilemapPointer2>tilemap-1 && tilemapPointer2<tilemap+16*8)tilemapPointer2=tilemapPointer;
             (*tilemapPointer2)=DEMON;
         }
-        tilemapPointer++;
+        tilemapPointer--;
     }
 }
 
 void main()
 {
-    // player
-    unsigned char playerTile=MECH_NONE;
-    unsigned char playerX=1;
+    unsigned char playerTile=MECH;
+    unsigned char playerX=3;
     unsigned char playerY=4;
 
-    // world
-    unsigned char scrollDelay=16;
-    unsigned char scrollDelayCounter=1;
-    tileset=tilesetFrame0;
-    for(unsigned char i=0;i<128;i++)tilemap[i]=EMPTY;
+
+    tileset=tilesetFrame0; // TODO: animate
+    for(unsigned char i=0;i<128;i++)tilemap[i]=EMPTY; // TODO: start ramp
 
     // main loop
     color = 0;
     unsigned char run=TRUE;
     while(run)
     {
-        tilemap[(playerY<<4)+playerX]=EMPTY;
+        //tilemap[(playerY<<4)+playerX]=EMPTY;
 
         unsigned char playerXOld=playerX;
         unsigned char playerYOld=playerY;
@@ -144,7 +186,7 @@ void main()
                     // TODO: bonus points
                 }
             }
-            else // damage collision
+            else if(tile>BULLET) // damage collision
             {
                 playerTile--;
                 if(playerTile<MECH_NONE)
@@ -154,11 +196,8 @@ void main()
             }
         }
 
-        if(key(KEY_A))tilemap[(playerY<<4)+playerX+1]=BULLET;
 
-        sim();
-        sim();
-
+/*
         scrollDelayCounter--;
         if(scrollDelayCounter==0)
         {
@@ -166,15 +205,23 @@ void main()
             scroll();
 
             tilemap[(rnd(7)<<4)+15]=DEMON;
-            tilemap[(rnd(7)<<4)+15]=WALL;
-            tilemap[(rnd(7)<<4)+15]=WALL;
-            tilemap[(rnd(7)<<4)+15]=WALL-rnd(2);
-        }
+            tilemap[(rnd(7)<<4)+15]=WALL-rnd(1);
+            //tilemap[(rnd(7)<<4)+15]=WALL-rnd(1);
 
+        }*/
 
-        tilemap[(playerY<<4)+playerX]=playerTile;
+        if(key(KEY_A))tilemap[(playerY<<4)+playerX]=BULLET;
+
+        sim();
+        sim();
+
+        //tilemap[(playerY<<4)+playerX]=playerTile;
+
 
         drawTilemap();
+
+        block(tileset+( playerTile<<3 ),playerX,playerY);
+
         lcd(screen);
     }
 
