924bf34d684a0cde030d7ab99b649e65bf4c9555
Nicholas Hull <interfectum@gmail.com>
2021-12-18 16:52:29 -0800

    Revert "Renamed directories"
    This reverts commit 2d6ab31311081504c00b8e9ba1250417e2ed232e.

---
+++ src/lib_test/rnd.h
@@ -0,0 +1,10 @@
+// randomizer definitions for z-fighter
+// by xrrawva 2021
+
+#ifndef RND_H
+#define RND_H
+
+unsigned char rnd(unsigned char maxValue);
+void rndImprove(unsigned char unpredictableByte);
+
+#endif // RND_H
