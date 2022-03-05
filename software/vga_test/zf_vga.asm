b0e2fd9fde9be9c912f35ce95c7afb6d4ca4e626
tangent3D <interfectum@gmail.com>
2022-03-04 16:22:44 -0800

    Update zf_vga.asm

--- software/vga_test/zf_vga.asm
+++ software/vga_test/zf_vga.asm
@@ -6,30 +6,31 @@ EXTERN 	CTRL
 
 SECTION code_user
 
-LD 		A,90h
+LD 		A,80h
 OUT 	(CTRL),A 	;Init PPI
 LD		DE,0 		;Initialize destination address
-LD 		BC,1536 	;Initialize byte counter
+LD 		BC,1530h 	;Initialize byte counter
 
 LOOP:
-	LD		A,0 		;Byte to write to screen
+	LD		A,255h		;Byte to write to screen
 	OUT		(PORTA),A
 
-	LD		A,D
+	LD 		A,D
+	OUT 	(PORTC),A
+
+	LD		A,E
 	OUT		(PORTB),A
-	LD		A,0+1 		;Latch LU
-	OUT		(CTRL),A
 
-	LD 		A,E
-	OUT 	(PORTB),A
-	LD 		A,2+1 		;Latch LL
-	OUT		(CTRL),A
+	LD  	A,9 	;WRDY active
+	OUT 	(CTRL),A
+	NOP
+	LD  	A,8 		;WRDY inactive
+	OUT 	(CTRL),A
 
-	LD 		A,90h
-	OUT 	(CTRL),A 	;Init PPI (should hopefully reset latches? may need to just reset them one by one for safety)
-
+	INC 	DE
 	DEC 	BC
 	LD      A,C
 	OR      B
 	JP		NZ,LOOP
-	RET
+
+RET
