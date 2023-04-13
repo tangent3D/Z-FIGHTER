5443131b6df910f37f27f15201f11a276fe47df3
tangent3D <interfectum@gmail.com>
2023-04-12 23:24:34 -0700

    Wait for CTS, poll SIO until CTS deasserted

--- software/zf_loader/zf_loader.asm
+++ software/zf_loader/zf_loader.asm
@@ -4,7 +4,7 @@
 ; For use with z80com (https://github.com/tangent3D/z80com).
 ; Copies boot loader to top of RAM, jumps to it and disables ROM.
 ; Loads a .BIN serially through SIO Ch.A to 0000h.
-; Uses RTS/CTS signals.
+; Z-FIGHTER JP3 must be set to 1-2 position.
 ; Initializes stack pointer beneath boot loader.
 ; Performs CALL to 0000h. RET to boot loader is supported.
 
@@ -12,9 +12,7 @@ EXTERN ROMDISABLE_n
 EXTERN SIO_A
 EXTERN SIO_AC
 
-RAMLOC      EQU     0FE00h        ; Destination address in RAM for boot loader (reserve 512 bytes)
-
-    LD      SP,RAMLOC-1
+RAMLOC      EQU  0FF00h            ; Destination address in RAM for boot loader (reserve 256 bytes)
 
 COPY:
     LD      HL,0000h               ; Copy boot loader ROM to RAM
@@ -26,78 +24,90 @@ COPY:
 ORG         RAMLOC
 
 LOADER:
+    LD      SP,RAMLOC-1            ; Initialize stack pointer below boot loader
+
     LD      A,0
     OUT     (ROMDISABLE_n),A       ; Disable ROM
 
-    CALL    INITSIO                ; Initialize SIO Ch.A
+    CALL    INIT_SIO               ; Initialize SIO Ch.A
 
-    CALL    WAIT_CTS               ; Wait for /CTS
+    CALL    WAIT_CTS               ; Wait for CTS
 
-    LD      HL,GREETING            ; Write greeting messages
-    CALL    STRING
+    LD      HL,GREETING            ; Write greeting message
+    CALL    TXSTRING
 
     CALL    RXDATA                 ; Receive and load data to 0000h
-    CALL    RTS_OFF                ; Deassert RTS when finished receiving data
+
     CALL    0000h                  ; CALL loaded data
     JP      LOADER                 ; Allow program to RET to boot loader
 
-INITSIO:
+; Subroutines
+
+INIT_SIO:
     LD      C,SIO_AC               ; Load C with SIO Ch.A control port
     LD      HL,INITCFG             ; Load HL with base address of config table
     LD      B,11                   ; Load B with length of config table
     OTIR                           ; Output configuration table to (C)
     RET
 
-STRING:
-    LD      C,SIO_AC
-ST1:
-    LD      A,(HL)                 ; Load character to be sent into A
-    OR      A                      ; Check if character = 0
-    JP      Z,CRLF                 ; If so, insert CR,LF and exit routine
-    CALL    TXCHAR                 ; Otherwise, transmit character
-    INC     HL                     ; Increment HL to next character in string
-    JP      ST1                    ; Repeat until string is terminated
-CRLF:
-    LD      A,0Dh                  ; Transmit CR
-    CALL    TXCHAR
-    LD      A,0Ah                  ; Transmit LF
-    CALL    TXCHAR
-    RET                            ; End of routine
+WAIT_CTS:
+    IN      A,(SIO_AC)             ; Read SIO RR0
+    BIT     5,A                    ; Test RR0 D5 (CTS)
+    JP      Z,WAIT_CTS             ; Wait until CTS set
+                                   ; Fall through
+REST_INT:
+    LD      A,00010000b            ; Reset ext/status interrupts (acknowledge CTS)
+    OUT     (SIO_AC),A
+    RET
 
-TXCHAR:
+TXSTRING:
     LD      C,SIO_AC
-TX1:
-    LD      D,0                    ; WR0, select WR0
-    OUT     (C),D
+TS1:
+    LD      A,(HL)                 ; Load character to be sent into A
+    OR      A                      ; Check for string terminator
+    RET     Z                      ; Return from routine if character is string terminator
+TS2:
     LD      B,1                    ; WR0, select RR1
     OUT     (C),B
     IN      B,(C)                  ; Read SIO RR1
     BIT     0,B                    ; Test RR1 D0 (All Sent)
-    JP      Z,TX1                  ; Wait until All Sent
+    JP      Z,TS2                  ; Wait until All Sent
     OUT     (SIO_A),A              ; Transmit character
-    RET
+    INC     HL                     ; Increment HL to next character in string
+    JP      TS1                    ; Repeat until string is terminated
 
 RXDATA:
     LD      HL,0000h               ; Point HL to bottom of address space
-    CALL    RTS_ON                 ; Assert RTS to indicate readiness to z80com
-READ:
-    LD      A,0                    ; Check if CTS active
-    OUT     (SIO_AC),A             ; WR0, select RR0
-    IN      A,(SIO_AC)
-    BIT     5,A
-    RET     Z                      ; Exit loop if CTS inactive (host closed serial connection)
-
-    LD      A,0                    ; WR0, select RR0
+    LD      A,00000101b            ; Assert RTS to indicate readiness to z80com:
+    OUT     (SIO_AC),A             ; WR0, select WR5
+    LD      A,01101010b            ; WR5, No DTR, TX 8 Bits/Character, No Send Break, Tx Enable, RTS on
     OUT     (SIO_AC),A
-    IN      A,(SIO_AC)             ; Read SIO RR0
-    BIT     0,A                    ; Test RR0 D0 (Rx Character Available)
-    JP      Z,READ                 ; Repeat routine if no character available
-
+RX1:
+    IN      A,(SIO_AC)
+    BIT     0,A                    ; Test RR0 D0 (Rx character available)
+    JP      NZ,RX2                 ; Receive character if available
+    BIT     5,A                    ; If not, check if CTS still active
+    JP      Z,RX3                  ; Exit loop if CTS inactive (host closed serial connection)
+    JP      RX1                    ; Repeat routine if no character available
+RX2:
     IN      A,(SIO_A)              ; Read available character
-    LD      (HL),A                 ; Load received character to (HL)
+    LD      (HL),A                 ; Load received character at memory address
     INC     HL
+    JP      RX1                    ; Repeat routine until CTS inactive
+RX3:
+    LD      A,L                    ; Reset loader if no bytes have been loaded
+    OR      H
+    JP      Z,LOADER
 
-    JP      READ                   ; Repeat routine until CTS inactive
+    LD      A,00000101b            ; Deassert RTS
+    OUT     (SIO_AC),A             ; WR0, select WR5
+    LD      A,01101000b            ; WR5, No DTR, TX 8 Bits/Character, No Send Break, Tx Enable, RTS off
+    OUT     (SIO_AC),A
+    CALL    REST_INT               ; Acknowledge CTS
+
+    RET                            ; Return to execute loaded data
+
+; Data
 
 INITCFG:
     DB      00011000b              ; WR0, Channel Reset
@@ -112,30 +122,5 @@ INITCFG:
     DB      00000101b              ; WR0, select WR5
     DB      01101000b              ; WR5, No DTR, TX 8 Bits/Character, No Send Break, Tx Enable, RTS off
 
-RTS_OFF:
-    LD      A,00000101b            ; WR0, select WR5
-    OUT     (SIO_AC),A
-    LD      A,01101000b            ; WR5, No DTR, TX 8 Bits/Character, No Send Break, Tx Enable, RTS off
-    OUT     (SIO_AC),A
-    RET
-
-RTS_ON:
-    LD      A,00000101b            ; WR0, select WR5
-    OUT     (SIO_AC),A
-    LD      A,01101010b            ; WR5, No DTR, TX 8 Bits/Character, No Send Break, Tx Enable, RTS on
-    OUT     (SIO_AC),A
-    RET
-
-WAIT_CTS:
-    LD      A,00000000b            ; WR0, select RR0
-    OUT     (SIO_AC),A
-    IN      A,(SIO_AC)
-    BIT     5,A
-    JP      Z,WAIT_CTS             ; Repeat until CTS is asserted
-    RET
-
 GREETING:
-    DB      "[Z-FIGHTER Serial Boot Loader v0.3]",0
-
-PAD:
-    DEFS    8000h-PAD              ; Pad binary size to 32K
+    DB      "[Z-FIGHTER Serial Boot Loader v0.3]",0Dh,0Ah,0
